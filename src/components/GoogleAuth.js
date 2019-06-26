c0e1ab9994da018c969cd62cacc91e73558177b2
johnpospisil <audiofreak7@gmail.com>
2019-06-26 15:22:13 -0700

    Add log in/out buttons

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -22,11 +22,15 @@ state = { isSignedIn: null };
 
 renderAuthButton() {
   if (this.state.isSignedIn === null) {
-    return <div>I don't know if we are signed in</div>;
+    return null;
   } else if (this.state.isSignedIn) {
-    return <div>I am signed in!</div>;
+    return (
+      <button className="ui red button"><i className="google icon"></i>Sign Out</button>
+      );
   } else {
-    return <div>I am not signed in</div>
+    return (
+      <button className="ui green button"><i className="google icon"></i>Sign In</button>
+      );
   }
 }
 
