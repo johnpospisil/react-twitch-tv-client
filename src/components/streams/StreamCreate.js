31c53b7776f0ecce13c55b9c6a8dc875a87eccd9
johnpospisil <audiofreak7@gmail.com>
2019-06-30 10:06:57 -0700

    Display validation error messages on the screen

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-renderInput({ input, label }) {
+renderInput({ input, label, meta }) {
+  console.log(meta);
   return (
     <div className="field">
       <label>{label}</label>
       <input {...input} />
+      <div>{meta.error}</div>
     </div>
     
   );
@@ -43,5 +45,6 @@ const validate = formValues => {
 };
 
 export default reduxForm({
-  form: 'streamCreate'
+  form: 'streamCreate',
+  validate
 })(StreamCreate);
