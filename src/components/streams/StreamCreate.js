65ed7429abf5fa44501df77f025cb23eaf359faf
johnpospisil <audiofreak7@gmail.com>
2019-07-01 10:18:02 -0700

    npm install axios redux-thunk. Create streams using an action creator.

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
 renderError({ error, touched }) {
@@ -28,6 +30,7 @@ renderInput = ({ input, label, meta }) => {
 
 onSubmit(formValues) {
   // console.log(formValues);
+
 }
 
   render() {
